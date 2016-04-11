57284baf315b9c2ab8367daf02de4cd32c3d94e7
Hanns Holger Rutz <contact@sciss.de>
2016-04-12 00:55:49 +0200

    sdc

---
+++ src/main/scala/de/sciss/unlike/SDC_Thumbnail.scala
@@ -0,0 +1,13 @@
+package de.sciss.unlike
+
+import de.sciss.file._
+
+object SDC_Thumbnail extends App {
+  import RCThumbnail._
+
+  fOut    = userHome/"Documents"/"applications"/"160411_SoundDevelopmentCity"/"latex"/"figures"/"sdc_imperfect_thumb.jpg"
+  rows    = 3
+  columns = 7
+
+  run()
+}
